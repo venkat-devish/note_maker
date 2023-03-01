82402e21def4581a23cc088f02cb34e229f1cd4d
venkat-devish <venkatsuryakiranv@gmail.com>
2023-03-01 08:31:10 +0530

    add: new note form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Home, NewNote } from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container className="my-4">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path="/:id">
+            <Route index element={<Home />} />
+            <Route path="edit" element={<Home />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/NewNote.tsx
@@ -0,0 +1,12 @@
+import { NoteForm } from "../components";
+
+const NewNote = () => {
+  return (
+    <>
+      <div>NewNote</div>
+      <NoteForm />
+    </>
+  );
+};
+
+export default NewNote;
